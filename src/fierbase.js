dca9be8d244ad984006d9c498534f26a20515fdc
Saurabh Bisht <76562057+sbisht01-dev@users.noreply.github.com>
2024-06-15 22:50:37 +0530

    test

--- src/fierbase.js
+++ src/fierbase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
+import { configDotenv } from "dotenv";
 const firebaseConfig = {
-  apiKey: "AIzaSyDEoIOdrFe2NPDUVxORGGTA7Ky8Y28Xefg",
+  apiKey: process.REACT_APP_API_KEY,
   authDomain: "getjob-2bdca.firebaseapp.com",
   projectId: "getjob-2bdca",
   storageBucket: "getjob-2bdca.appspot.com",
   messagingSenderId: "338998159739",
   appId: "1:338998159739:web:042319b66c6d427b80b5de",
-  measurementId: "G-9YLB73PZ2Q"
+  measurementId: "G-9YLB73PZ2Q",
 };
 
 export const app = initializeApp(firebaseConfig);
-
