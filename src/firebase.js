30cda678e7fbb495386cde06cdf9e56f5ef6e26d
Saurabh Bisht <76562057+sbisht01-dev@users.noreply.github.com>
2024-07-03 05:41:11 +0000

    env_added

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
-  apiKey: "AIzaSyDEoIOdrFe2NPDUVxORGGTA7Ky8Y28Xefg",
-  authDomain: "getjob-2bdca.firebaseapp.com",
-  projectId: "getjob-2bdca",
-  storageBucket: "getjob-2bdca.appspot.com",
-  messagingSenderId: "338998159739",
-  appId: "1:338998159739:web:042319b66c6d427b80b5de",
-  measurementId: "G-9YLB73PZ2Q"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUKCET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID,
 };
 export const app = initializeApp(firebaseConfig);
